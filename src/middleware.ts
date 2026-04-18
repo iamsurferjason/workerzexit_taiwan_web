e734880eec7353e4b9045aeacceaffd0bbe29037
iamsurferjason <iloveoliverchan@gmail.com>
2026-04-18 11:02:02 +0800

    Read secure auth cookie in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,6 +11,7 @@ export async function middleware(req: NextRequest) {
     const token = await getToken({
       req,
       secret: process.env.NEXTAUTH_SECRET,
+      secureCookie: req.nextUrl.protocol === 'https:',
     })
     if (!token) {
       if (isApiAdmin) {
